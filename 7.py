56f4a0efdb6c84681798ab27a3efe5ff22cebf1b
dayone3536-cmyk <dayone3536@gmail.com>
2026-02-23 07:05:56 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,115 @@
+'''
+SATART.
+TAKE THE USER INPUT ON x OR o DSIPLAY THE BOARD AND CALCULATE WINNER
+TAKE INPUT HOW()
+FIRST ASK THE USR TO ENTER A NUMBER BETWEEN 0 TO 8 AND IF THAT IS INVALID TRY AGAIN FROM THE USER
+DISPLAYING USERS INPUT IN THE BOARD??
+---------------------------------------------FIRS TKAE THE INPUT NAD PRITN AFTER EACH MOVE THEN TAKE THAT INPUT AND GIVE IT A INDEX IN BOARD'''
+        # I WANT TO DISPLAY USERS INPUT IN THE BOARD HOW.
+'''
+        FIRST. TAKE THE USER INPUT AND GIVE IT A POSITION IN THE BOARD 
+        HOW TO CONVERT USER INPUT INTO A STR AND PLACE IT IN THE BOARDD
+        HOW TO CONVERT TO A STR>
+        take the user input and make it a str
+        NOW GIVE A POSITION TO X IN BOARD                            
+'''
+'''
+        '''
+        # I WANT TO MAKE A COMBIANTION OF WINING PATTERS
+        # HOW. 
+        # MAKE A LSIT AND INSIDE TUPLES OF WINING COMBINATIONS
+        #         # '''
+        # I HAVE TO AKE THE BAORD CONSTANT RATHER THAN CHNGING DUE TO THE WHILE LOOP FIXED✅✅
+
+        # I HAVE TO DECLARE THE WINNERR 
+        # HOW.
+        # FIRST MAKE A WINNING COMBINATIONS OUTSIDE THE LOOP SO IT DOESNT CHANGES EVERY TIME
+
+
+        # CHECK WHO WON 
+        # CHECKS DRAW
+        # WHO WON HOW.
+        # first make winning combinations
+        # THEN CHECK IF THE USERS INPUTS HAVE SATISFED THE COMBO ELSE PRINT ITS A DRAW 
+'''
+        winning_combo = [(0,3,6) , (1,4,7) , (2,5,8) , (0,1,2) , (3,4,5) , (6,7,8) , (0,4,8) , (2,4,6)]
+        if X in winning_combo:
+            pass
+        if O in winning_combo:
+            pass
+            ''' 
+
+def boardd():
+    board = [" "] * 9
+
+    while 2 > 1:
+        X = int(input("X's turn (0-8) 10 to quit: "))
+        if X == 10:
+            print("OK BYE")
+            break
+        if X > 8:
+            print("too high: ")
+            continue
+        if X < 0:
+            print("too low! try again!: ")
+            continue
+        board[X] = "X" #board is the set of str and in that i am assinging X value index to be the str X instaed of an empty space
+        print(board[0] ,  "|" , board [1] ,  "|" , board[2] , "|" )
+        print("--------------")
+        print(board[3] ,  "|" , board[4] , "|" , board[5] , "|" )
+        print("--------------")
+        print(board[6] ,  "|" , board[7] ,  "|" , board[8] , "|" )
+
+        if board[0] == board[1] == board[2] != " ":
+                print("X WINS!!")
+        if board[3] == board[4] == board[5] != " ":
+                print("X WINS!!")
+        if board[6] == board[7] == board[8] != " ":
+                print("X WINS!!")
+        if board[0] == board[3] == board[6] != " ":
+               print("X WINS!!")
+        if board[1] == board[4] == board[7] != " ":
+               print("X WINS!!")
+        if board[2] == board[5] == board[8] != " ":
+               print("X WINS!!")
+        if board[0] == board[4] == board[8] != " ":
+               print("X WINS!!")
+        if board[2] == board[4] == board[6] != " ":
+              print("X WINS!!")
+
+        O = int(input("O's turn enter a number (0-8) 10 to quit"))
+        if O == 10:
+                print("OK")
+                break  
+        if O > 8:
+                print("too high: ")
+                continue
+        if O < 0:
+                print("too low!: ")
+                continue
+
+        board[O] = "O"
+        print(board[0] , "|" , board[1] , "|" , board[2] , "|" )
+        print("--------------")
+        print(board[3] , "|" , board[4] , "|" , board[5] , "|" )
+        print("--------------")
+        print(board[6] , "|" , board[7] , "|" , board[8] , "|" )
+        
+        if board[0] == board[1] == board[2] != " ":
+                print("O WINS!!")
+        if board[3] == board[4] == board[5] != " ":
+                print("O WINS!!")
+        if board[6] == board[7] == board[8] != " ":
+                print("O WINS!!")
+        if board[0] == board[3] == board[6] != " ":
+               print("O WINS!!")
+        if board[1] == board[4] == board[7] != " ":
+               print("O WINS!!")
+        if board[2] == board[5] == board[8] != " ":
+               print("O WINS!!")
+        if board[0] == board[4] == board[8] != " ":
+               print("O WINS!!")
+        if board[2] == board[4] == board[6] != " ":
+              print("O WINS!!")
+
+boardd()
